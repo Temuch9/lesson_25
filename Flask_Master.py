677e061dbb60a713675ca33ec2eb9a782b32f6bc
Temuch9 <152795475+Temuch9@users.noreply.github.com>
2024-05-05 18:16:03 +0300

    Add files via upload

--- Flask_Master.py
+++ Flask_Master.py
@@ -3,12 +3,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    main_data = {
+        'Город': 'Чебоксары',
+        'Район': 'Московский',
+        'Компания': 'Iserv'
+    }
+    context = {
+        'name': 'Артём',
+        'age': '17',
+        'spes': 'Пчеловод'
+
+    }
+    return render_template('index.html', main_data=main_data, **context)
 
 @app.route("/contacts/")
 def contacts():
     developer_name = 'Ilon_Mask'
     return render_template('contacts.html', name=developer_name)
+
+@app.route("/results/")
+def results():
+    data = ['phyton', 'js', 'G0', 'c#', 'C++', 'lua', 'React', 'SQL', 'Postgesql']
+    return render_template('results.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
